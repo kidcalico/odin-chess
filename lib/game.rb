d7b6fd922ba264e971205bfda393db037a332d3d
Kid Calico <43305187+kidcalico@users.noreply.github.com>
2026-03-24 19:39:42 -0700

    Combine Game and LoadGame

--- lib/game.rb
+++ lib/game.rb
@@ -4,21 +4,38 @@ require_relative 'load_game'
 require_relative 'player'
 
 class Game
-  attr_accessor :board, :game_stats
+  NEW_GAME = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'.freeze
+  TEST_GAME = '1r6/5pp1/R1R4p/1r1pP3/2pkQPP1/7P/1P6/2K5 w - - 0 41'
+  # SAVED_GAME = 'r1bqkbnr/ppp2ppp/2np4/4p3/3PP3/5N2/PPP2PPP/RNBQKB1R w KQkq - 2 4'
 
-  def initialize
-    @game = LoadGame.new
-    @board = Board.new(@game.matrix_notation)
-    @game_stats = @game.game_stats
-    load_stats
+  attr_accessor :game, :game_stats, :board_array, :board
+
+  def initialize(fen_code = TEST_GAME)
+    @game_array = load_fen(fen_code)
+    @board_array = @game[0]
+    @board = Board.new(board_array)
+    @game_stats = load_stats
+  end
+
+  def load_fen(fen_code)
+    result = fen_code.split(' ')
+    result[0] = num_to_empties(result[0].split('/').map { |block| block.split('') })
+    result
   end
 
   def load_stats
-    @turn = @game_stats[:turn]
-    @castle = @game_stats[:castle]
-    @en_passant = @game_stats[:en_passant]
-    @half_moves = @game_stats[:half_moves]
-    @full_moves = @game_stats[:full_moves]
+    { turn: game[1], castle: game[2], en_passant: game[3],
+      half_moves: game[4], full_moves: game[5] }
+  end
+
+  def num_to_empties(fen_code)
+    fen_code.map do |rank|
+      rank.map do |square|
+        next [nil] * square.to_i if /^[1-8]$/.match?(square)
+
+        square
+      end.flatten
+    end
   end
 
   def play_game
@@ -35,6 +52,8 @@ class Game
 end
 
 test = Game.new
-test.board.print_board('b')
-p test.board.board[7][3].symbol
-p test.board.board[1][0].legal_moves
+
+p test.board_array
+p test.board
+test.board.print_board('w')
+p test.game_stats
