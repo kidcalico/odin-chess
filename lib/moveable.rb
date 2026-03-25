7ba4696250deeb675745085b43ef9bf3ea7d9182
Kid Calico <43305187+kidcalico@users.noreply.github.com>
2026-03-25 16:32:21 -0700

    Refactor all pawn methods and knight methods

--- lib/moveable.rb
+++ lib/moveable.rb
@@ -1,8 +1,8 @@
 module Moveable
-  def possible_moves(board_array)
-    case type
-    when 'pawn' then pawn_moves(board_array)
-    when 'knight' then knight_moves(board_array)
+  def possible_moves(location, board_array, turn)
+    case board_array[location[0]][location[1]].type
+    when 'pawn' then pawn_moves(location, board_array, turn)
+    when 'knight' then knight_moves(location, board_array, turn)
     when 'bishop' then bishop_moves(board_array)
     when 'rook' then rook_moves(board_array)
     when 'queen' then queen_moves(board_array)
@@ -20,24 +20,28 @@ module Moveable
     # checks for attack possibilities,
   end
 
-  def pawn_moves(board_array)
-    return white_pawn_moves if color == 'white'
+  def pawn_moves(location, board_array, turn)
+    if turn == 'white' && board_array[location[0]][location[1]].color == 'white'
+      return white_pawn_moves(location,
+                              board_array, turn)
+    end
 
-    black_pawn_moves
+    black_pawn_moves(location, board_array, turn) if turn == 'black'
   end
 
-  def white_pawn_moves(board_array)
+  def white_pawn_moves(location, board_array, turn)
     result = []
-    if location[0] == 6
-      result.push([location[0] - 1, location[1]],
-                  [location[0] - 2, location[1]])
-    elsif location[0] > 0
-      result = [location[0] - 1, location[1]]
-      unless location[1] - 1 < 0 && board_array[location[0] - 1][location[1] - 1].nil? && board_array[location[0] - 1][location[1] - 1].color == 'white'
+    if location[0] > 0
+      result.push([location[0] - 1, location[1]]) if board_array[location[0] - 1][location[1]].nil?
+      if location[0] == 6 && board_array[location[0] - 1][location[1]].nil? && board_array[location[0] - 1][location[1]].nil?
+        result.push([location[0] - 2,
+                     location[1]])
+      end
+      if location[1] - 1 > 0 && !board_array[location[0] - 1][location[1] - 1].nil? && board_array[location[0] - 1][location[1] - 1].color == 'black'
         result.push([location[0] - 1,
                      location[1] - 1])
       end
-      unless location[1] + 1 > 7 && board_array[location[0] - 1][location[1] + 1].nil? && board_array[location[0] - 1][location[1] + 1].color == 'white'
+      if location[1] + 1 < 7 && !board_array[location[0] - 1][location[1] + 1].nil? && board_array[location[0] - 1][location[1] + 1].color == 'black'
         result.push([location[0] - 1,
                      location[1] + 1])
       end
@@ -45,18 +49,19 @@ module Moveable
     result
   end
 
-  def black_pawn_moves(board_array)
+  def black_pawn_moves(location, board_array, turn)
     result = []
-    if location[0] == 1
-      result.push([location[0] + 1, location[1]],
-                  [location[0] + 2, location[1]])
-    elsif location[0] < 7
-      result = [location[0] + 1, location[1]]
-      unless location[1] - 1 < 0 && board_array[location[0] + 1][location[1] - 1].nil? && board_array[location[0] + 1][location[1] - 1].color == 'black'
+    if location[0] < 7
+      result.push([location[0] + 1, location[1]]) if board_array[location[0] + 1][location[1]].nil?
+      if location[0] == 1 && board_array[location[0] + 1][location[1]].nil? && board_array[location[0] + 2][location[1]].nil?
+        result.push([location[0] + 2,
+                     location[1]])
+      end
+      if location[1] - 1 > 0 && !board_array[location[0] + 1][location[1] - 1].nil? && board_array[location[0] + 1][location[1] - 1].color == 'white'
         result.push([location[0] + 1,
                      location[1] - 1])
       end
-      unless location[1] + 1 > 7 && board_array[location[0] + 1][location[1] + 1].nil? && board_array[location[0] + 1][location[1] + 1].color == 'black'
+      if location[1] + 1 < 7 && !board_array[location[0] + 1][location[1] + 1].nil? && board_array[location[0] + 1][location[1] + 1].color == 'white'
         result.push([location[0] + 1,
                      location[1] + 1])
       end
@@ -64,7 +69,13 @@ module Moveable
     result
   end
 
-  def knight_moves
+  def knight_moves(location, board_array, turn)
+    all_knight_moves(location).map do |move|
+      move if board_array[move[0]][move[1]].nil? || board_array[move[0]][move[1]].color != turn
+    end.compact
+  end
+
+  def all_knight_moves(location)
     [
       [location[0] + 1, location[1] + 2], [location[0] - 1, location[1] + 2],
       [location[0] + 1, location[1] - 2], [location[0] - 1, location[1] - 2],
